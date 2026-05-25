276b11ec59fc5dab3607a68c547537997aa130a6
murad <muradkv@gmail.com>
2026-05-25 22:04:08 +0300

    Initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by murad on 25.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
